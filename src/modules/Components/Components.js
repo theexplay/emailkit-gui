cf607f7077e6c976299f9ee1ee9592be8f75f8a6
Murat Atila <matila@ivi.ru>
2017-11-04 00:55:45 +0300

    drag and drop test

--- src/modules/Components/Components.js
+++ src/modules/Components/Components.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
+
 class Components extends React.Component {
+
+
     render() {
         const components = ['Spacer', 'Table', 'Buttons', 'Text', 'Images'];
 
@@ -30,3 +33,25 @@ class Components extends React.Component {
 }
 
 export default Components;
+
+
+{/*<div className="Components">
+    <div className="Components__title">
+        <span>ADD</span>
+    </div>
+    <div className="Components__description">Click and drag elemenets over</div>
+    <div className="Components__list">
+        {components.map((cmp, key) => {
+            return (
+                <div className="Components__item" key={key}>
+                    <div className="Components__item-container">
+                        <div className="Components__item-image">
+                            <img src={`modules/Components/images/${cmp}.jpg`} alt={cmp}/>
+                        </div>
+                        <div className="Components__item-text">{cmp}</div>
+                    </div>
+                </div>
+            )
+        })}
+    </div>
+</div>*/}
--- src/modules/Constructor/Constructor.js
+++ src/modules/Constructor/Constructor.js
@@ -1,8 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Components from '../Components/Components';
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+
+const getItems = count =>
+    Array.from({length: count}, (v, k) => k).map(k => ({
+        id: `item-${k}`,
+        content: `item ${k}`,
+    }));
+
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
+const grid = 8;
+const getItemStyle = (draggableStyle, isDragging) => Object.assign(
+    {
+        // some basic styles to make the items look a bit nicer
+        userSelect: 'none',
+        padding: grid * 2,
+        marginBottom: grid,
+        // change background colour if dragging
+        background: isDragging ? 'lightgreen' : 'grey',
+
+    },
+    // styles we need to apply on draggables
+    draggableStyle
+);
+
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    padding: grid,
+    marginBottom: grid,
+    width: 250,
+});
 
 class Constructor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: getItems(10),
+        };
+        this.onDragEnd = this.onDragEnd.bind(this);
+    }
+
+    onDragEnd(result) {
+        console.log(result)
+
+        // dropped outside the list
+        if (!result.destination) {
+            return;
+        }
+
+        const items = reorder(
+            this.state.items,
+            result.source.index,
+            result.destination.index
+        );
+
+        this.setState({
+            items,
+        });
+    }
+
     render() {
         return (
             <div className="Constructor">
@@ -13,6 +77,7 @@ class Constructor extends React.Component {
                 </div>
                 <div className="Constructor__main">
                     <div className="Constructor__area">
+
                     </div>
                 </div>
             </div>
